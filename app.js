30428fa0ae5d5685aaec5d90d5b388a394d5580f
Lynnette Ng <lynnette-ng@outlook.com>
2016-05-05 04:55:58 +0000

    Add penguin fact data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var request = require("request");
 
 var config = require("./config");
+var data = require("./data");
 
 var app = express();
 
@@ -26,6 +27,8 @@ app.get('/', function (req, res) {
   var str = "how about some pizza";
   console.log(encodeURI(str));
   console.log(req.get('host'));
+  
+  console.log(data.hungryStrings);
 
 });
 
@@ -73,6 +76,9 @@ app.post('/webhook/', function (req, res) {
   res.sendStatus(200);
 });
 
+/* 
+* This function gets intent calls from Wit.ai
+*/
 function getRequest(callback) {
   console.log("text " + text);
   request({
@@ -84,7 +90,18 @@ function getRequest(callback) {
   }, function(error, response, body){
       if(!error && response.statusCode == 200) {
         var bodyjsonparse = JSON.parse(body);
-        var intent = bodyjsonparse["outcomes"][0]["entities"]["intent"][0]["value"];
+        
+        var entities = bodyjsonparse["outcomes"][0]["entities"];
+        var intent;
+        console.log(entities);
+        if (entities.length == 0) {
+          intent = "";
+        } else if (entities["intent"] == undefined) {
+          intent = "";
+        } else {
+          intent = entities["intent"][0]["value"];
+        }
+        
         console.log("intent " + intent);
         
         executeIntent(intent);
@@ -93,6 +110,9 @@ function getRequest(callback) {
   });
 }
 
+/*
+* Executes the intents 
+*/
 function executeIntent(intent) {
   console.log("Calling intent " + intent);
   
@@ -105,15 +125,18 @@ function executeIntent(intent) {
       break;
     case "picture":
       console.log("Picture");
-      sendPenguinPhoto(userid);
+      sendPenguinPhoto();
       break;
     case "fact":
-      sendPenguinFact(userid);
+      sendPenguinFact();
     case "food":
       sendFoodMessage();
       break;
     case "goodbye":
       sendGoodbye();
+      break;
+    case "praise":
+      sendThanks();
       break;
     default:
       sendDefault();
@@ -121,7 +144,7 @@ function executeIntent(intent) {
   }   
 }
 
-function sendPenguinPhoto(recipientId) {
+function sendPenguinPhoto() {
   console.log("photo message");
     
   var messageData = {
@@ -140,7 +163,7 @@ function sendPenguinPhoto(recipientId) {
           }, 
           {
             "type": "postback",
-            "title": "Okay, enough distraction",
+            "title": "Enough distraction",
             "payload": "goodbye",
           }],
         }]
@@ -148,20 +171,22 @@ function sendPenguinPhoto(recipientId) {
     }
   };
 
-    sendMessage(recipientId, messageData);
+  sendMessage(messageData);
 };
 
-function sendPenguinFact(recipientId) {
+function sendPenguinFact() {
   console.log("penguin fact message");
-    
+  
+  var randomFact = data.factStrings[Math.floor(Math.random()*data.factStrings.length)];
+
   var messageData = {
     "attachment": {
       "type": "template",
       "payload": {
         "template_type": "generic",
         "elements": [{
-          "title": "Penguin Fact #1",
-          "subtitle": "About 95% of Galapagos penguins, the most northernly of all penguin species, is found along the western coast of Isabela and around Fernandina Island.",
+          "title": "Did You Know?",
+          "subtitle": randomFact,
           "buttons": [{
             "type": "postback",
             "title": "Cool, Next Fact!",
@@ -169,7 +194,7 @@ function sendPenguinFact(recipientId) {
           },
           {
             "type": "postback",
-            "title": "Okay, enough distraction",
+            "title": "Enough distraction",
             "payload": "goodbye"
             }],
         }]
@@ -177,34 +202,80 @@ function sendPenguinFact(recipientId) {
     }
   };
 
-    sendMessage(recipientId, messageData);
+  sendMessage(messageData);
 };
+
+function sendFoodMessage() {
+  console.log("penguin food message");
+  
+  var rand = Math.random();
+  
+  var messageData;
+  
+  if (rand > 0.5) {
+    var randomHungryMessage = data.hungryStrings[Math.floor(Math.random()*data.hungryStrings.length)];
+    messageData = {"text": randomHungryMessage};
+  } 
+  else {
+    messageData = {
+      "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "Penguin's Hungry Too!",
+          "subtitle": "OMNOMNOM",
+          "buttons": [{
+            "type": "postback",
+            "title": "Still Hungry!",
+            "payload": "food"
+          },
+          {
+            "type": "postback",
+            "title": "I'm full",
+            "payload": "goodbye"
+            }],
+        }]
+      }
+      }
+    };
+  }
+
+  sendMessage(messageData);  
+}
 
 function showHelp() {
   var message = {text: "Penguins are naturally blur. Type 'picture' for cute penguin pictures, " +
               "'fact' for penguin trivia, 'joke' for some laughter and 'food' for hungry penguins"
   };
-  sendMessage(userid, message);
+  sendMessage(message);
 }
 
 function sendGreeting() {
   var message = {text: "Hello, Penguin here! What would you like to do today?\n" +
                 "How about a penguin picture, fact, joke or food?"
   };
-  sendMessage(userid, message);
-}
-
-function sendFoodMessage() {
-  
+  sendMessage(message);
 }
 
 function sendDefault() {
+  var message = "I'm sorry, I don't understand you. Type 'help' to see what Penguin can do!";
+  sendMessage(message);
   
+  var intents = ['picture', 'food', 'fact', 'joke'];
+  var randomIntent = intents[Math.floor(Math.random()*intents.length)];
+  executeIntent(randomIntent);
+}
+
+function sendThanks() {
+  // Can include a penguin dancing gif
+  var message = {text: "Thank you!"};
+  sendMessage(message);
 }
 
 function sendGoodbye() {
   var message = {text: "Goodbye! Penguin will miss you!"};
-  sendMessage(userid, message);
+  sendMessage(message);
 }
 
 /*
@@ -213,13 +284,13 @@ function sendGoodbye() {
 * If just sending a text it's {text: text}
 */
 
-function sendMessage(recipientId, message) {
+function sendMessage(message) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: config.access_token},
         method: 'POST',
         json: {
-            recipient: {id: recipientId},
+            recipient: {id: userid},
             message: message,
         }
     }, function(error, response, body) {
---
+++ data.js
@@ -0,0 +1,26 @@
+var data = {};
+
+data.hungryStrings = [
+  'How about a pizza?',
+  'I\'m hungry too!'
+];
+
+data.hungryPics = [
+];
+
+data.factStrings = [
+    'About 95% of Galapagos penguins, the most northernly of all penguin species, is found along the western coast of Isabela and around Fernandina Island.',
+    'All 18 species of penguins live in the Southern Hemisphere.',
+    'Penguins are found on every continent in the Southern Hemisphere.',
+    'Penguins are abundant on many temperate and subantarctic islands.',
+    'Penguins generally live on islands and remote continental regions free from land predators, where their inability to fly is not detrimental to their survival.',
+    'Penguins are usually found near nutrient-rich, cold-water currents that provide an abundant supply of food.',
+    'The most southerly penguin colony in the world are a group of Adelies that regularly nest near Camp Royds, Antarctica.',
+    
+];
+
+data.pictures = {
+    'Baby Penguin #1': 'img/babypenguin1.jpg'
+};
+
+module.exports = data;
